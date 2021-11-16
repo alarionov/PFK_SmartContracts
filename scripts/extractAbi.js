8a98536dbfe6bcdd2e2d658764cae898a0dfbdc5
Larionov <artem.larionov@here.com>
2021-11-15 18:16:11 -0600

    WIP

---
+++ scripts/extractAbi.js
@@ -0,0 +1,30 @@
+async function extractABI(contractName) 
+{
+    const artifactsPath = `browser/contracts/artifacts/${contractName}.json`;
+    const abiPath = `browser/contracts/ABI/${contractName}.json`;
+    
+    console.log(`Extracting Abi from ${artifactsPath} to ${abiPath}`);
+    
+    const metadata = JSON.parse(await remix.call('fileManager', 'getFile', artifactsPath));
+    await remix.call("fileManager", "setFile", abiPath, JSON.stringify(metadata.abi));
+}
+
+(async () => {
+    const contracts = [
+        "AuthContract",
+        "RandomContract",
+        "CharacterContract",
+        "FightContract",
+        "FightManagerContract",
+        "EquipmentContract",
+        "EquipmentManagerContract",
+        "Act1Milestones",
+        "Act1Sidequests"
+    ];
+    
+    for (let i = 0; i < contracts.length; ++i)
+    {
+        const contractName = contracts[i];
+        await extractABI(contractName);
+    }
+})();
